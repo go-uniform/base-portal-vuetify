dbfa22c98650681372112537316ed37f89f3c07f
Justin Robertson <justinr@uprate.co.za>
2022-04-19 19:59:32 +0200

    Enhanced system translation abilities

---
+++ src/locale/base/af.ts
@@ -0,0 +1,110 @@
+import af from 'vuetify/src/locale/af';
+const phrases: any = af;
+
+phrases.base = {
+  validations: {
+    required: 'Mag nie leeg wees nie',
+    email: 'Moet \'n geldige e-posadres wees',
+    length: 'Mag nie meer as {0} karakters lank wees nie.',
+  },
+  errors: {
+    unknownOtpType: 'Onbekende OTP tipe \'{0}\' bespeur',
+    incorrectOtpPin: 'Verkeerde OTP pin verskaf',
+  },
+  app: {
+    title: 'Uniform',
+    headerTitle: 'Uniform',
+    logoAlt: 'Uniform',
+    logoUrl: 'https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png',
+    smallLogoAlt: 'Uniform',
+    smallLogoUrl: 'https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png',
+    lightMode: 'Ligte Wyse',
+    darkMode: 'Donker Wyse',
+    logout: 'Teken Uit',
+    mainMenu: 'Hooflys',
+    backLogin: 'Terug na aanmelding',
+    backSafety: 'Terug na veiligheid',
+    save: 'Skep',
+    cancel: 'Kanselleer',
+    agreeTitle: 'Ja',
+    disagreeTitle: 'Nee',
+    deleteConfirmationTitle: 'Is jy seker?',
+    deleteConfirmationMessage: 'Is jy seker jy wil die geselekteerde rekord uitvee?',
+  },
+  logout: {
+    pageTitle: 'Teken Uit',
+    pageHtml: 'Jy is suksesvol van die stelsel afgemeld.',
+  },
+  login: {
+    pageTitle: 'Teken In',
+    title: 'Teken in op jou rekening',
+    identifier: 'Gebruikersnaam of E-pos',
+    password: 'Wagwoord',
+    button: 'Teken In',
+    forgot: 'Wagwoord vergeet?'
+  },
+  otp: {
+    pageTitle: 'OTP',
+    title: 'Voer jou eenmalige pin (OTP) in',
+    validate: 'Valideer',
+    resend: 'Stuur Weer',
+    resendConfirmationTitle: 'Is jy seker?',
+    resendConfirmationMessage: 'Herstuur sal \'n nuwe kode genereer en die voorheen gestuurde kode ongeldig maak, gebruik dit slegs as jy nie die kode betyds ontvang het nie. Is jy steeds seker jy wil weer vir jouself \'n nuwe kode stuur?',
+  },
+  passwordReset: {
+    pageTitle: 'Herstel Wagwoord',
+    title: 'Herstel jou rekening wagwoord',
+    identifier: 'Gebruikersnaam of E-pos',
+    button: 'HerStel',
+  },
+  home: {
+    pageTitle: 'Tuis',
+  },
+  entity: {
+    list: 'Lys {0}',
+    new: 'Nuwe {0}',
+    edit: 'Wysig {0}',
+    view: 'Beskou {0}',
+  },
+  entityList: {
+    headerActions: 'Aksies',
+    buttonNew: 'Nuwe',
+    buttonEdit: 'Wysig',
+    buttonView: 'Beskou',
+    buttonDelete: 'Skrap',
+    buttonBulk: 'Maat',
+    bulkActionConfirmationTitle: 'Is jy seker?',
+    bulkActionConfirmationMessage: 'Jy is op die punt om {0} oor {1} rekord(s) uit te voer, is jy seker jy wil dit nog doen?',
+  },
+  entityView: {
+    list: 'Lys',
+    view: 'Beskou',
+    edit: 'Wysig',
+    delete: 'Skrap',
+  },
+  entityEdit: {
+    new: 'Nuwe',
+    view: 'Beskou',
+    edit: 'Wysig',
+  },
+  entityFilters: {
+    title: 'Filtreer',
+  },
+  notFound: {
+    pageTitle: 'Bladsy Nie Gevind Nie',
+    pageHtml: 'Die bladsy waarna jy soek kan nie gevind word nie.',
+  },
+  cookieConsent: {
+    popUpTitle: 'Your privacy',
+    popUpBody: 'By clicking "Accept all cookies", you agree we may store cookies on your device and disclose information in accordance with our <a href="/policies/cookie">Cookie Policy.<a/>',
+    popUpBtn: 'Accept all cookies',
+    pageTitle: 'Cookie Consent',
+    pageHtml: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet sem dapibus, placerat orci a, euismod lacus. Mauris eget eros id ex convallis dictum sit amet vel felis. Donec lectus quam, mattis quis velit faucibus, congue finibus metus. In neque mi, tristique nec mattis ac, tristique vel dolor. Donec posuere justo orci, congue vehicula nibh luctus sit amet. Integer mi mi, dapibus ultrices tortor et, sollicitudin posuere sapien. Nulla facilisi. Proin congue arcu vel mattis molestie. Phasellus felis quam, tincidunt non eros eu, tempus sodales ligula. In euismod consectetur porta.</p>',
+  },
+  connection: {
+    pageTitle: '',
+    pageHtml: '<p class="display-2">You are offline.</p><p>Try:</p><ul><li>Turning off airplane mode</li><li>Turning on mobile data or Wi-Fi</li><li>Checking the signal in your area</li></ul><br><p>This message will automatically close once your connection is restored.</p>'
+  }
+};
+
+export default phrases;
--- src/locale/en.ts
+++ src/locale/en.ts
@@ -1,117 +1,38 @@
-import en from 'vuetify/src/locale/en';
+import en from '@/locale/base/en';
 const phrases: any = en;
 
-phrases.raw = {
-  'Users': 'Users',
-  'User': 'User',
-  'User Roles': 'User Roles',
-  'User Role': 'User Role',
-}
-
 phrases.custom = {
-  validations: {
-    required: 'May not be empty',
-    email: 'Must be a valid email address',
-    length: 'May not be more than {0} characters long.',
+  users: {
+    singular: 'User',
+    plural: 'Users',
+    sections: {
+      general: 'General',
+    },
+    fields: {
+      id: 'Id',
+      firstName: 'First Name',
+      lastName: 'Last Name',
+      username: 'Username',
+      email: 'Email',
+      userRoleId: 'User Role',
+      modifiedAt: 'Modified At',
+      createdAt: 'Created At',
+    },
   },
-  errors: {
-    unknownOtpType: 'Unknown OTP type \'{0}\' detected',
-    incorrectOtpPin: 'Incorrect OTP supplied',
+  userRoles: {
+    singular: 'User Role',
+    plural: 'User Roles',
+    sections: {
+      general: 'General',
+    },
+    fields: {
+      id: 'Id',
+      name: 'Email',
+      description: 'Description',
+      modifiedAt: 'Modified At',
+      createdAt: 'Created At',
+    },
   },
-  app: {
-    title: 'Uniform',
-    headerTitle: 'Uniform',
-    logoAlt: 'Uniform',
-    logoUrl: 'https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png',
-    smallLogoAlt: 'Uniform',
-    smallLogoUrl: 'https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png',
-    lightMode: 'Light Mode',
-    darkMode: 'Dark Mode',
-    logout: 'Log Out',
-    mainMenu: 'Main Menu',
-    backLogin: 'Back to login',
-    backSafety: 'Back to safety',
-    save: 'Save',
-    cancel: 'Cancel',
-    agreeTitle: 'Yes',
-    disagreeTitle: 'No',
-    deleteConfirmationTitle: 'Are you sure?',
-    deleteConfirmationMessage: 'Are you sure you wish to delete the selected record?',
-  },
-  logout: {
-    pageTitle: 'Logout',
-    pageHtml: 'You have been successfully logged out of the system.',
-  },
-  login: {
-    pageTitle: 'Login',
-    title: 'Log into your account',
-    identifier: 'Username or Email',
-    password: 'Password',
-    button: 'Login',
-    forgot: 'Forgot Password?'
-  },
-  otp: {
-    pageTitle: 'OTP',
-    title: 'Enter your one time pin (OTP)',
-    validate: 'Validate',
-    resend: 'Resend',
-    resendConfirmationTitle: 'Are you sure?',
-    resendConfirmationMessage: 'Resending will generate a new code and invalidate the previously sent code, only use this if you have not received the code in a timely fashion. Are you still sure you wish to resend yourself a new code?',
-  },
-  passwordReset: {
-    pageTitle: 'Reset Password',
-    title: 'Reset your account password',
-    identifier: 'Username or Email',
-    button: 'Reset',
-  },
-  home: {
-    pageTitle: 'Home',
-  },
-  entity: {
-    list: 'List {0}',
-    new: 'New {0}',
-    edit: 'Edit {0}',
-    view: 'View {0}',
-  },
-  entityList: {
-    title: 'List',
-    headerActions: 'Actions',
-    buttonNew: 'New',
-    buttonEdit: 'Edit',
-    buttonView: 'View',
-    buttonDelete: 'Delete',
-    buttonBulk: 'Bulk',
-    bulkActionConfirmationTitle: 'Are you sure?',
-    bulkActionConfirmationMessage: 'You are about to {0} {1} record(s), are you sure you want to do this?',
-  },
-  entityView: {
-    list: 'List',
-    view: 'View',
-    edit: 'Edit',
-    delete: 'Delete',
-  },
-  entityEdit: {
-    new: 'New',
-    view: 'View',
-    edit: 'Edit',
-  },
-  entityFilters: {
-    title: 'Filter',
-  },
-  notFound: {
-    pageTitle: 'Page Not Found',
-    pageHtml: 'The page you are looking for can\'t be found.',
-  },
-  cookieConsent: {
-    pageTitle: 'Cookie Consent',
-    pageHtml: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet sem dapibus, placerat orci a, euismod lacus. Mauris eget eros id ex convallis dictum sit amet vel felis. Donec lectus quam, mattis quis velit faucibus, congue finibus metus. In neque mi, tristique nec mattis ac, tristique vel dolor. Donec posuere justo orci, congue vehicula nibh luctus sit amet. Integer mi mi, dapibus ultrices tortor et, sollicitudin posuere sapien. Nulla facilisi. Proin congue arcu vel mattis molestie. Phasellus felis quam, tincidunt non eros eu, tempus sodales ligula. In euismod consectetur porta.</p>',
-    popUpTitle: 'Your privacy',
-    popUpBody: 'By clicking "Accept all cookies", you agree we may store cookies on your device and disclose information in accordance with our <a href="/policies/cookie">Cookie Policy.<a/>',
-    popUpBtn: 'Accept all cookies',
-  },
-  connection: {
-    pageHtml: '<p class="display-2">You are offline.</p><p>Try:</p><ul><li>Turning off airplane mode</li><li>Turning on mobile data or Wi-Fi</li><li>Checking the signal in your area</li></ul><br><p>This message will automatically close once your connection is restored.</p>'
-  }
 };
 
 export default phrases;
