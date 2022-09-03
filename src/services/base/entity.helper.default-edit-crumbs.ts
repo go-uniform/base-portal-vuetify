2713fa702f6e74644947914326f0517c0cd9076a
Justin Robertson <justinr@uprate.co.za>
2022-09-03 20:38:27 +0200

    enhanced default edit/view crumbs construction to take into account the repository location being the same as the root location to avoid duplication in the crumbs

--- src/services/base/entity.helper.default-edit-crumbs.ts
+++ src/services/base/entity.helper.default-edit-crumbs.ts
@@ -2,38 +2,40 @@ import {translate} from '@/plugins/base/vuetify';
 import {IRepository} from '@/services/base/global.interfaces';
 
 export const defaultEditCrumbs = (repository: IRepository<any>, id: string | null | undefined) => {
-  if (id) {
-    return [
-      {
-        icon: translate('$vuetify.home.icon'),
-        title: translate('$vuetify.home.pageTitle'),
-        location: '/',
-      },
-      {
-        title: translate(repository.title.plural),
-        location: repository.listPage,
-      },
-      {
-        title: translate('$vuetify.entityEdit.view'),
-        location: `${translate(repository.viewPage,id)}`,
-      },
-      {
-        title: translate('$vuetify.entityEdit.edit'),
-      },
-    ];
-  }
-  return [
+  const crumbs: any = [
     {
       icon: translate('$vuetify.home.icon'),
       title: translate('$vuetify.home.pageTitle'),
       location: '/',
-    },
-    {
+    }
+  ];
+
+  if (repository.listPage != '/') {
+    crumbs.push({
       title: translate(repository.title.plural),
       location: repository.listPage,
-    },
-    {
-      title: translate('$vuetify.entityEdit.new'),
-    },
-  ];
+    });
+  }
+
+  if (id) {
+    crumbs.push(
+      {
+        title: translate('$vuetify.entityEdit.view'),
+        location: `${translate(repository.viewPage,id)}`,
+      }
+    );
+    crumbs.push(
+      {
+        title: translate('$vuetify.entityEdit.edit'),
+      }
+    );
+  } else {
+    crumbs.push(
+      {
+        title: translate('$vuetify.entityEdit.new'),
+      }
+    );
+  }
+
+  return crumbs;
 };
--- src/services/base/entity.helper.default-view-crumbs.ts
+++ src/services/base/entity.helper.default-view-crumbs.ts
@@ -2,18 +2,24 @@ import {translate} from '@/plugins/base/vuetify';
 import {IRepository} from '@/services/base/global.interfaces';
 
 export const defaultViewCrumbs = (repository: IRepository<any>) => {
-  return [
+  const crumbs: any = [
     {
       icon: translate('$vuetify.home.icon'),
       title: translate('$vuetify.home.pageTitle'),
       location: '/',
-    },
-    {
+    }
+  ];
+
+  if (repository.listPage != '/') {
+    crumbs.push({
       title: translate(repository.title.plural),
       location: repository.listPage,
-    },
-    {
-      title: translate('$vuetify.entityView.view'),
-    },
-  ];
+    });
+  }
+
+  crumbs.push({
+    title: translate('$vuetify.entityView.view'),
+  });
+
+  return crumbs;
 };
