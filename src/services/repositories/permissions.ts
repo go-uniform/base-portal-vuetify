63e8abdfe97e197dabb0c01be8999e9d3b25b649
Justin Robertson <justinr@uprate.co.za>
2023-03-03 12:59:30 +0200

    [#18] Refactor src/services/repositories/permissions.ts to use the latest structured repository setup

--- src/services/repositories/permissions.ts
+++ src/services/repositories/permissions.ts
@@ -1,99 +1,74 @@
-import {IAttribute, IRepository} from '@/services/base/global.interfaces';
+import {IAttribute, Repository} from '@/services/base/global.interfaces';
 import {EnumValueType, EnumHeaderAlign} from '@/services/base/global.enums';
 import {Section} from '@/services/base/global.classes.section';
-import {baseBulk} from '@/services/base/entity.bulk';
-import {baseCreate, baseDelete, baseList, baseRead, baseUpdate} from '@/services/base/entity.crud';
 
 const entity = 'permissions';
 const slug = 'permissions';
 
-export const permissions: IRepository<IAttribute> = {
-  freeTextSearch: true,
-  entity: entity,
-  slug: slug,
-  title: {
-    singular: `$vuetify.${entity}.singular`,
-    plural: `$vuetify.${entity}.plural`,
-  },
-  defaultSortOrder: '-createdAt',
-  listPage: `/${slug}`,
-  addPage: `/${slug}/add`,
-  viewPage: `/${slug}/view/{0}`,
-  editPage: `/${slug}/edit/{0}`,
-  fields: {
-    id: {
-      label: `$vuetify.${entity}.fields.id`,
-      type: EnumValueType.Uuid,
-      readonly: true,
-    },
-    type: {
-      label: `$vuetify.${entity}.fields.type`,
-      type: EnumValueType.Enumeration,
-      defaultValue: 'inherit',
-      values: [
-        {
-          value: 'inherit',
-          title: `$vuetify.${entity}.enums.type.inherit.title`,
-          icon: `$vuetify.${entity}.enums.type.inherit.icon`,
-          color: `$vuetify.${entity}.enums.type.inherit.color`,
-        },
-        {
-          value: 'allow',
-          title: `$vuetify.${entity}.enums.type.allow.title`,
-          icon: `$vuetify.${entity}.enums.type.allow.icon`,
-          color: `$vuetify.${entity}.enums.type.allow.color`,
-        },
-        {
-          value: 'deny',
-          title: `$vuetify.${entity}.enums.type.deny.title`,
-          icon: `$vuetify.${entity}.enums.type.deny.icon`,
-          color: `$vuetify.${entity}.enums.type.deny.color`,
-        },
-      ],
-    },
-    key: {
-      label: `$vuetify.${entity}.fields.key`,
-      type: EnumValueType.Text,
-      pattern: /^[a-zA-z0-9]+$/,
-      patternMessage: '$vuetify.validations.jsonKey',
-    },
-    name: {
-      label: `$vuetify.${entity}.fields.name`,
-      type: EnumValueType.Text,
-    },
-    description: {
-      label: `$vuetify.${entity}.fields.description`,
-      type: EnumValueType.TextArea,
-      optional: true,
-    },
-    modifiedAt: {
-      label: `$vuetify.${entity}.fields.modifiedAt`,
-      type: EnumValueType.DateTime,
-      readonly: true,
-    },
-    createdAt: {
-      label: `$vuetify.${entity}.fields.createdAt`,
-      type: EnumValueType.DateTime,
-      readonly: true,
-      filterable: true,
-    },
-  },
-  headers: [
+const repository = new Repository<IAttribute>(slug, {}, {});
+
+repository.addField('id', {
+  label: '',
+  type: EnumValueType.Uuid,
+  readonly: true,
+});
+repository.addField('type', {
+  label: '',
+  type: EnumValueType.Enumeration,
+  defaultValue: 'inherit',
+  values: [
     {
-      fieldKey: 'key',
+      value: 'inherit',
+      title: `$vuetify.${entity}.enums.type.inherit.title`,
+      icon: `$vuetify.${entity}.enums.type.inherit.icon`,
+      color: `$vuetify.${entity}.enums.type.inherit.color`,
     },
     {
-      fieldKey: 'name',
+      value: 'allow',
+      title: `$vuetify.${entity}.enums.type.allow.title`,
+      icon: `$vuetify.${entity}.enums.type.allow.icon`,
+      color: `$vuetify.${entity}.enums.type.allow.color`,
     },
     {
-      fieldKey: 'description',
-    },
-    {
-      fieldKey: 'createdAt',
-      align: EnumHeaderAlign.End,
+      value: 'deny',
+      title: `$vuetify.${entity}.enums.type.deny.title`,
+      icon: `$vuetify.${entity}.enums.type.deny.icon`,
+      color: `$vuetify.${entity}.enums.type.deny.color`,
     },
   ],
-  sections: [
+});
+repository.addField('key', {
+  label: '',
+  type: EnumValueType.Text,
+  pattern: /^[a-zA-z0-9]+$/,
+  patternMessage: '$vuetify.validations.jsonKey',
+});
+repository.addField('name', {
+  label: '',
+  type: EnumValueType.Text,
+});
+repository.addField('description', {
+  label: '',
+  type: EnumValueType.Text,
+});
+
+repository.setHeaders([
+  {
+    fieldKey: 'key',
+  },
+  {
+    fieldKey: 'name',
+  },
+  {
+    fieldKey: 'description',
+  },
+  {
+    fieldKey: 'createdAt',
+    align: EnumHeaderAlign.End,
+  },
+]);
+
+repository.addSection(
     new Section(`$vuetify.${entity}.sections.general`, [
       'id',
       'type',
@@ -102,21 +77,16 @@ export const permissions: IRepository<IAttribute> = {
       'description',
       'modifiedAt',
       'createdAt',
-    ]),
-  ],
-  bulkActions: [
-    {
-      color: 'error',
-      icon: 'mdi-delete',
-      title: '$vuetify.entityList.delete',
-      key: 'delete'
-    }
-  ],
+    ])
+);
 
-  list: baseList<IAttribute>(slug),
-  create: baseCreate<IAttribute>(slug),
-  read: baseRead<IAttribute>(slug),
-  update: baseUpdate<IAttribute>(slug),
-  delete: baseDelete<IAttribute>(slug),
-  bulk: baseBulk(slug),
-};
+repository.bulkActions = [
+  {
+    color: 'error',
+    icon: 'mdi-delete',
+    title: '$vuetify.entityList.delete',
+    key: 'delete'
+  }
+];
+
+export const permissions = repository;
