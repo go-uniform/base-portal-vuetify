ac6913acad4feae6c3112124079669b4b573b8de
Justin Robertson <justinrza@gmail.com>
2025-05-18 22:47:46 +0200

    updated report image placeholders

--- src/services/stubs/reports.ts
+++ src/services/stubs/reports.ts
@@ -9,7 +9,7 @@ import {Report, reports} from '@/services/repositories/reports';
 export const ReportsList: Report[] = baseListLoad([
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report1`,
     urlEdit: `https://en.wikipedia.org/w/index.php?title=Main_course&action=history`,
     title: 'Report #1',
@@ -19,7 +19,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report2`,
     title: 'Report #2',
     description: 'This is a description',
@@ -28,7 +28,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report3`,
     title: 'Report #3',
     description: 'This is a description',
@@ -37,7 +37,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report4`,
     title: 'Report #4',
     description: 'This is a description',
@@ -46,7 +46,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report5`,
     title: 'Report #5',
     description: 'This is a description',
@@ -55,7 +55,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report6`,
     title: 'Report #6',
     description: 'This is a description',
@@ -64,7 +64,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report7`,
     title: 'Report #7',
     description: 'This is a description',
@@ -73,7 +73,7 @@ export const ReportsList: Report[] = baseListLoad([
   },
   {
     id: generateUuid(),
-    urlThumbnail: 'https://via.placeholder.com/800x600',
+    urlThumbnail: 'https://placehold.co/800x600',
     urlView: `https://en.wikipedia.org/wiki/Report8`,
     title: 'Report #8',
     description: 'This is a description',
@@ -84,7 +84,7 @@ export const ReportsList: Report[] = baseListLoad([
 
 const handlers = baseHandlers(reports);
 handlers['POST /reports'] = baseCreateStub(reports, (record: any) => {
-  record.urlThumbnail = 'https://via.placeholder.com/800x600';
+  record.urlThumbnail = 'https://placehold.co/800x600';
   record.urlView = `https://en.wikipedia.org/wiki/${encodeURIComponent(record.title)}`;
   record.urlEdit = `https://en.wikipedia.org/w/index.php?title=${encodeURIComponent(record.title)}&action=history`;
   return record;
